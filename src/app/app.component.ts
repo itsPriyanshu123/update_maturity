3289fdaee0a665e422ed3159b8fedc928bd7a142
itsPriyanshu123 <priyanshujee2017@gmail.com>
2024-06-24 00:37:19 -0700

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ViewQuoteComponent } from './view-quote/view-quote.component';
+import { QuoteDetailsComponent } from './quote-details/quote-details.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ReactiveFormsModule,ViewQuoteComponent],
+  imports: [RouterOutlet,ReactiveFormsModule,ViewQuoteComponent,QuoteDetailsComponent,CustomerDetailsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/quote-details/quote-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './quote-details.component.html',
+  styleUrl: './quote-details.component.css'
+})
+export class QuoteDetailsComponent {
+
+}

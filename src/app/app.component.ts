08490ea67e934b71f3772181c8fe2c9cd9e32bc1
itsPriyanshu123 <priyanshujee2017@gmail.com>
2024-06-23 23:53:56 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ViewQuoteComponent } from './view-quote/view-quote.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReactiveFormsModule,ViewQuoteComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'form-ui';
+ isChildren:boolean = false;
+
+
+
 }
